95b9a162b5b8d66ca1086be55d2d32ecad42456d
Luke Metz <luke.metz@students.olin.edu>
2012-12-05 17:58:25 -0500

    Client update

--- client.py
+++ client.py
@@ -7,6 +7,10 @@ import serial
 import threading
 import os
 import time
+import struct
+
+textport = "8080"
+host = "piquopter.olin.edu"
 
 #Class just to print out whats on the socket.
 class Reader(threading.Thread):
@@ -14,16 +18,77 @@ class Reader(threading.Thread):
 		super(Reader, self).__init__()
 		self.socket = socket
 	def run(self):
+		count = 0
+		data = ""
+
 		while 1:
-			print self.socket.recv(128);
+			count +=1
+			self.fil = open(str(count)+"moo.jpg", 'wb+')
+			l = []
+			if host == "localhost":
+				chars = self.socket.recv(8)
+				l = struct.unpack('<Q', chars)
+			else:
+				#f (count == 1):
+				chars = self.socket.recv(4)
+				otherfi = open(str(count)+"size.hex", 'wb+')
+				otherfi.write(chars)
+				otherfi.close()
+				l = struct.unpack('<L', chars)
+				'''else:
+					chars = self.socket.recv(32)
+					#chars += self.socket.recv(32)
+					print "DOIN THE THING"
+					otherfi = open(str(count)+"size.hex", 'wb+')
+					otherfi.write(chars)
+					otherfi.close()'''
+			print l
+			#bad state for l if too big
+			b5 = ''
+			b4 = ''
+			b3 = ''
+			b2 = ''
+			b1 = ''
+			b0 = ''
+			upper = 0
+			while (l[0] > 50000):
+				upper += 1
+				if (upper%100 == 0):
+					print upper
+				b0 = self.socket.recv(1)
+				if (b1 == chr(0xff) and b0 == chr(0xd8)):
+					print "asdfasdf", ord(b2), ord(b3), ord(b1), ord(b0)
+					l = struct.unpack('<L',  str(b5) + str(b4) + str(b3) + str(b2))
+					print "found new l", l
+
+				b5 = b4
+				b4 = b3
+				b3 = b2
+				b2 = b1
+				b1 = b0
+
+			read_at_a_time = 8
+			bytes_left = l[0]
+
+			while bytes_left > 0:
+				if bytes_left > read_at_a_time:
+					self.fil.write(self.socket.recv(read_at_a_time));
+				else:
+					self.fil.write(self.socket.recv(bytes_left));
+				bytes_left -= read_at_a_time
+				#print bytes_left
+			print "Done with frame!"
+			
 
 def main():
-	textport = "8080"
-	host = "localhost"
+	
+
+	#host = "localhost"
 	port = int(textport)
 	addr = (host, port)
 	buf = 1024
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 	sock.connect(addr)
 	sock.send("C1200");
 	r = Reader(sock)
